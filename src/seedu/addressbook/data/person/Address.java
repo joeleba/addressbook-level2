881e3e7d01d9051876118e57e9c741936cb6a5e2
joeleba <hieu.giang.2810@gmail.com>
2016-08-28 21:57:57 +0800

    update equals method

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -66,7 +66,7 @@ public class Address {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
