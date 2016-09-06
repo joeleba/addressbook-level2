c8000751edd9f977516841237eee99aebeb3c07c
joeleba <hieu.giang.2810@gmail.com>
2016-09-06 15:58:52 +0800

    initial commit

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.parser.Parser;
+
+public class NameTest {
+
+}
