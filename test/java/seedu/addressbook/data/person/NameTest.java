613ccdfa542daaf614f3213041124bd64a401d42
joeleba <hieu.giang.2810@gmail.com>
2016-09-06 17:06:19 +0800

    tests

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -6,10 +6,56 @@ import org.junit.Before;
 import org.junit.Test;
 
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.tag.Tag;
-import seedu.addressbook.data.tag.UniqueTagList;
-import seedu.addressbook.parser.Parser;
 
 public class NameTest {
+    private Name thisName;
+    
+    @Before
+    public void setup() {
+        thisName = generateTestName("Jon Snow");
+    }
+    
+    @Test
+    public void isSimilar_nullOtherName() {
+        Name otherName = null; 
+        assertFalse(thisName.isSimilar(otherName));
+    }
+    
+    @Test
+    public void isSimilar_differentCases() {
+        Name otherName = generateTestName("jon snow"); 
+        assertTrue(thisName.isSimilar(otherName));
+    }
+    
+    @Test
+    public void isSimilar_totallyDifferent() {
+        Name otherName = generateTestName("Tormund"); 
+        assertFalse(thisName.isSimilar(otherName));
+    }
+    
+    @Test
+    public void isSimilar_sameInitials() {
+        Name otherName = generateTestName("Jim Snow");
+        assertTrue(thisName.isSimilar(otherName));
+    }
+ 
+    @Test
+    public void isSimilar_sameInitialsReversed() {
+        Name otherName = generateTestName("Snow Jim");
+        assertFalse(thisName.isSimilar(otherName));
+    }
+    
+    @Test
+    public void isSimilar_sameName() {
+        Name otherName = generateTestName("Jon Snow");
+        assertTrue(thisName.isSimilar(otherName));
+    }
 
+    private static Name generateTestName(String name) {
+        try {
+            return new Name(name);
+        } catch (IllegalValueException ive) {
+            throw new RuntimeException("test name should be valid by definition");
+        }
+    }
 }
